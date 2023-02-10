427f3c9514d37985396c643d9733daf1e8b853d5
ptdulion-iup <cyky@iup.edu>
2023-02-09 23:59:53 -0500

    Working on encoder -> layer shift

--- keyboards/sofle/keymaps/sofle_penguin/keymap.c
+++ keyboards/sofle/keymaps/sofle_penguin/keymap.c
@@ -1,14 +1,21 @@
 #include QMK_KEYBOARD_H
 
 enum sofle_layers {
-    /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
     _QWERTY,
     _NUMBER,
     _GAME
 };
 
+enum custom_keycodes {
+    KC_QWERTY = SAFE_RANGE,
+    KC_NUMBER,
+    KC_GAME
+};
+
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_QWERTY] = LAYOUT(
+	[0] = LAYOUT(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                   KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                   KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
         KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,                  KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
@@ -16,14 +23,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL, KC_LGUI, KC_LCTL, KC_LALT, KC_LBRC, KC_SPC,     KC_ENT, KC_LEFT, KC_UP, KC_DOWN, KC_RGHT
         ),
 
-	[_NUMBER] = LAYOUT(
+	[1] = LAYOUT(
         KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                           KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
         KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5,                               KC_6, KC_7, KC_8, KC_9, KC_0, KC_F12,
         KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
         KC_TRNS, KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TRNS,        KC_TRNS, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                         KC_TRNS, MO(0), KC_TRNS, KC_TRNS, KC_TRNS
         ),
-	[_GAME] = LAYOUT(
+	[2] = LAYOUT(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                   KC_PSLS, KC_PAST, KC_PMNS, KC_F10, KC_F11, KC_F12,
         KC_TAB, KC_NO, KC_Q, KC_W, KC_E, KC_R,                  KC_P7, KC_P8, KC_P9, KC_F7, KC_F8, KC_F9,
         KC_LSFT, KC_NO, KC_A, KC_S, KC_D, KC_F,                 KC_P4, KC_P5, KC_P6, KC_F4, KC_F5, KC_F6,
@@ -46,7 +53,7 @@ static void render_logo(void) {
 
 static void print_status_narrow(void) {
     // Print current mode
-    oled_write_P(PSTR("\n\n"), false);
+    oled_write_P(PSTR("\n"), false);
     oled_write_ln_P(PSTR("Pengu"), false);
     oled_write_ln_P(PSTR(""), false);
     // if (keymap_config.swap_lctl_lgui) {
@@ -57,10 +64,10 @@ static void print_status_narrow(void) {
 
     switch (get_highest_layer(default_layer_state)) {
         case _QWERTY:
-            oled_write_ln_P(PSTR("Qwrt"), false);
+            oled_write_ln_P(PSTR("Qwrty"), false);
             break;
         case _NUMBER:
-            oled_write_ln_P(PSTR("Nmbr"), false);
+            oled_write_ln_P(PSTR("Numb"), false);
             break;
         case _GAME:
             oled_write_ln_P(PSTR("Game"), false);
@@ -68,15 +75,15 @@ static void print_status_narrow(void) {
         default:
             oled_write_P(PSTR("Undef"), false);
     }
-    oled_write_P(PSTR("\n\n"), false);
+    oled_write_P(PSTR("\n"), false);
     // Print current layer
     oled_write_ln_P(PSTR("LAYER"), false);
     switch (get_highest_layer(layer_state)) {
         case _QWERTY:
-            oled_write_P(PSTR("Base\n"), false);
+            oled_write_P(PSTR("Qwrty\n"), false);
             break;
         case _NUMBER:
-            oled_write_P(PSTR("Number\n"), false);
+            oled_write_P(PSTR("Numb\n"), false);
             break;
         case _GAME:
             oled_write_P(PSTR("Game\n"), false);
@@ -84,7 +91,7 @@ static void print_status_narrow(void) {
         default:
             oled_write_ln_P(PSTR("Undef"), false);
     }
-    oled_write_P(PSTR("\n\n"), false);
+    oled_write_P(PSTR("\n"), false);
     led_t led_usb_state = host_keyboard_led_state();
     oled_write_ln_P(PSTR("CPSLK"), led_usb_state.caps_lock);
     oled_write_ln_P(PSTR("NUM"), led_usb_state.num_lock);
@@ -108,10 +115,41 @@ bool oled_task_user(void) {
 
 #endif
 
+
+
 layer_state_t layer_state_set_user(layer_state_t state) {
     return update_tri_layer_state(state, _QWERTY, _NUMBER, _GAME);
 }
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_QWERTY:
+            if (record->event.pressed) {
+                // set_single_persistent_default_layer(_QWERTY);
+                // tap_code(TO(_QWERTY));
+                layer_on(_QWERTY);
+            }
+            layer_off(_QWERTY);
+            return false;
+        case KC_NUMBER:
+            if (record->event.pressed) {
+                // set_single_persistent_default_layer(_NUMBER);
+                // tap_code(TO(_NUMBER));
+                layer_on(_NUMBER);
+            }
+            layer_off(_NUMBER);
+            return false;
+        case KC_GAME:
+            if (record->event.pressed) {
+                // set_single_persistent_default_layer(_GAME);
+                // tap_code(TO(_GAME));
+                layer_on(_GAME);
+            }
+            layer_off(_GAME);
+            return false;
+    }
+    return true;
+}
 
 #ifdef ENCODER_ENABLE
 uint8_t layer = 0;
@@ -120,20 +158,35 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     // left
     if (index == 0) {
         if (clockwise) {
+            // tap_code(KC_VOLU);
+        // } else {
+        //     tap_code(KC_VOLD);
+        // }
             if (layer == _GAME) {
-                layer = _QWERTY;
+                // layer = _QWERTY;
+                layer = _GAME;
             } else {
                 layer++;
             }
         } else {
             if (layer == _QWERTY){
-                layer = _GAME;
+                layer = _QWERTY;
+                // layer = _GAME;
             } else {
                 layer--;
             }
         }
+
+        switch (layer) {
+            case _QWERTY:
+                tap_code16(KC_QWERTY);
+            case _NUMBER:
+                tap_code16(KC_NUMBER);
+            case _GAME:
+                tap_code16(KC_GAME);
+        }
         tap_code(TO(layer));
-    
+
     // right
     } else if (index == 1) {
         if (clockwise) {

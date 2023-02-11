5740a3f1621e7648df72b41ab5154252eaf7b42f
ptdulion-iup <cyky@iup.edu>
2023-02-10 19:17:29 -0500

    detect split keeb

--- keyboards/sofle/keymaps/sofle_penguin/config.h
+++ keyboards/sofle/keymaps/sofle_penguin/config.h
@@ -1,5 +1,12 @@
 #pragma once
-
+#define SPLIT_USB_DETECT
+#define MASTER_LEFT
+#define TAPPING_FORCE_HOLD
+#ifdef TAPPING_TERM
+    #undef TAPPING_TERM
+    #define TAPPING_TERM 200
+#endif
+#define ENCODER_DIRECTION_FLIP
 /* The way how "handedness" is decided (which half is which),
 see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.

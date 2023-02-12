2013a54cc854d4d595aac71f0da1838c51c52114
ptdulion-iup <cyky@iup.edu>
2023-02-11 21:45:30 -0500

    Changing layout and right encoder to volume on qwerty

--- keyboards/sofle/keymaps/sofle_penguin/keymap.c
+++ keyboards/sofle/keymaps/sofle_penguin/keymap.c
@@ -25,8 +25,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
 
 	[1] = LAYOUT(
-        KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                           KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-        KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5,                               KC_6, KC_7, KC_8, KC_9, KC_0, KC_F12,
+        KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                           KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
+        KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5,                               KC_6, KC_7, KC_8, KC_9, KC_0, KC_F12,
         KC_TRNS, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
         KC_TRNS, KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, KC_TRNS,        KC_TRNS, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, KC_TRNS,
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                         KC_TRNS, MO(0), KC_TRNS, KC_TRNS, KC_TRNS
@@ -181,7 +181,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         // } else {
         //     tap_code(KC_VOLD);
         // }
-            if (layer == _GAME * 3) {
+            if (layer == _GAME) {
                 layer = _QWERTY;
                 // layer = _GAME;
             } else {
@@ -213,10 +213,19 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 
     // right
     } else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
+
+        if (layer > 0) {
+            if (clockwise) {
+                tap_code(KC_VOLU);
+            } else {
+                tap_code(KC_VOLD);
+            }
         } else {
-            tap_code(KC_PGUP);
+            if (clockwise) {
+                tap_code(KC_PGDN);
+            } else {
+                tap_code(KC_PGUP);
+            }
         }
     }
     return true;
